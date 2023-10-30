01e2eadceb4f3032ca8b45cb3219dda911b1be8d
Hanna Achenbach <hanna.achenbach@aiven.io>
2023-10-30 15:21:52 +0100

    add radix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,105 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'; // This is a client component 👈🏽
+import '@radix-ui/themes/styles.css';
+import {Button, Flex, Link, Text} from '@radix-ui/themes';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import React, {useState} from 'react';
+import {HamburgerMenuIcon, DotFilledIcon, CheckIcon, ChevronRightIcon} from '@radix-ui/react-icons';
 
 export default function Home() {
+  const [bookmarksChecked, setBookmarksChecked] = useState(true);
+  const [urlsChecked, setUrlsChecked] = useState(false);
+  const [person, setPerson] = useState('pedro');
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <Flex direction="column" gap="2" align="start">
+        <Text>Can we build a DropdownMenu?</Text>
+        <Link href="https://www.radix-ui.com/primitives/docs/components/dropdown-menu">
+          https://www.radix-ui.com/primitives/docs/components/dropdown-menu
+        </Link>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <Button aria-label="Customise options">
+              <HamburgerMenuIcon />
+            </Button>
+          </DropdownMenu.Trigger>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+              <DropdownMenu.Item className="DropdownMenuItem">
+                New Tab <div className="RightSlot">⌘+T</div>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="DropdownMenuItem">
+                New Window <div className="RightSlot">⌘+N</div>
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className="DropdownMenuItem" disabled>
+                New Private Window <div className="RightSlot">⇧+⌘+N</div>
+              </DropdownMenu.Item>
+              <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
+                  More Tools
+                  <div className="RightSlot">
+                    <ChevronRightIcon />
+                  </div>
+                </DropdownMenu.SubTrigger>
+                <DropdownMenu.Portal>
+                  <DropdownMenu.SubContent className="DropdownMenuSubContent" sideOffset={2} alignOffset={-5}>
+                    <DropdownMenu.Item className="DropdownMenuItem">
+                      Save Page As… <div className="RightSlot">⌘+S</div>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="DropdownMenuItem">Create Shortcut…</DropdownMenu.Item>
+                    <DropdownMenu.Item className="DropdownMenuItem">Name Window…</DropdownMenu.Item>
+                    <DropdownMenu.Separator className="DropdownMenu.Separator" />
+                    <DropdownMenu.Item className="DropdownMenuItem">Developer Tools</DropdownMenu.Item>
+                  </DropdownMenu.SubContent>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Sub>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+              <DropdownMenu.Separator className="DropdownMenuSeparator" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+              <DropdownMenu.CheckboxItem
+                className="DropdownMenuCheckboxItem"
+                checked={bookmarksChecked}
+                onCheckedChange={setBookmarksChecked}
+              >
+                <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                  <CheckIcon />
+                </DropdownMenu.ItemIndicator>
+                Show Bookmarks <div className="RightSlot">⌘+B</div>
+              </DropdownMenu.CheckboxItem>
+              <DropdownMenu.CheckboxItem
+                className="DropdownMenuCheckboxItem"
+                checked={urlsChecked}
+                onCheckedChange={setUrlsChecked}
+              >
+                <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                  <CheckIcon />
+                </DropdownMenu.ItemIndicator>
+                Show Full URLs
+              </DropdownMenu.CheckboxItem>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+              <DropdownMenu.Separator className="DropdownMenuSeparator" />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <DropdownMenu.Label className="DropdownMenuLabel">People</DropdownMenu.Label>
+              <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
+                <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="pedro">
+                  <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                    <DotFilledIcon />
+                  </DropdownMenu.ItemIndicator>
+                  Pedro Duarte
+                </DropdownMenu.RadioItem>
+                <DropdownMenu.RadioItem className="DropdownMenuRadioItem" value="colm">
+                  <DropdownMenu.ItemIndicator className="DropdownMenuItemIndicator">
+                    <DotFilledIcon />
+                  </DropdownMenu.ItemIndicator>
+                  Colm Tuite
+                </DropdownMenu.RadioItem>
+              </DropdownMenu.RadioGroup>
+
+              <DropdownMenu.Arrow className="DropdownMenuArrow" />
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+      </Flex>
     </main>
-  )
+  );
 }
